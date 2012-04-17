508e4e4aa5e0df710c5578cbec0d2dded588d31a
Trent <trent@trent-desktop.(none)>
2012-04-17 09:53:07 -0700

    WIP;

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.codehaus.jackson/jackson-mapper-asl "1.9.5"]
                  [nl.siegmann.epublib/epublib-core "3.0-SNAPSHOT"]
-                 [noir-async "1.0.0-SNAPSHOT"]]
+                 [noir-async "1.0.0-SNAPSHOT"]
+                 [org.clojure/core.cache "0.5.0"]]
   :plugins [[lein-swank "1.4.4"]]
   :repositories {"psiegman-snapshots" "https://github.com/psiegman/mvn-repo/raw/master/snapshots"}
   :warn-on-reflection true
